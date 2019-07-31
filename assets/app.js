e17b7839501e2a6ce61a7db403a7f351934cbb9c
Dan Gold <tentativechaos@gmail.com>
2019-07-31 16:59:51 -0700

    Removed firebaseUI code as authentication have been moved to signin.html.

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// import * as firebaseui from 'firebaseui'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDAyKN-HiFo3_uMhRZERYu8SAZC6ZITARc",
+    authDomain: "multiplayer-rps-7330d.firebaseapp.com",
+    databaseURL: "https://multiplayer-rps-7330d.firebaseio.com",
+    projectId: "multiplayer-rps-7330d",
+    storageBucket: "multiplayer-rps-7330d.appspot.com",
+    messagingSenderId: "99026760332",
+    appId: "1:99026760332:web:559107199d351108"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
