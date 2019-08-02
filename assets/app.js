6eab280373a62396add220b23c0b5812847d5512
Dan Gold <tentativechaos@gmail.com>
2019-08-01 18:28:07 -0700

    Wrote a solveGame function to decide the winner of a two player game of rps.

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,34 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+
+// Create a handle for firebase data interaction
+var database = firebase.database();
+
+function solveGame (u1, u2) {
+    // Take two objects and returns the object of the winner, or undefined if there is no winner
+    // Inputs must be in the form of {user: username, move: move}
+    switch (u1.move) {
+        case rock:
+            if (u2.move = rock) {return;}
+            else if (u2.move = paper) {return u2}
+            else if (u2.move = scissors) {return u1};
+
+        case paper:
+            if (u2.move = rock) {return u1}
+            else if (u2.move = paper) {return}
+            else if (u2.move = scissors) {return u2};
+
+        case scissors:
+            if (u2.move = rock) {return u2}
+            else if (u2.move = paper) {return u1}
+            else if (u2.move = scissors) {return};
+    
+        default:
+            throw('Invalid input to solveGame. Moves must either be rock, paper, or scissors');
+    }
+};
